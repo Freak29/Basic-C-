143a17f6bf602a8f164b2b8360817ae2b00e7e3a
shradha07082001 <75068199+shradha07082001@users.noreply.github.com>
2021-10-03 13:44:05 +0530

    Create Code_for_swap_without_using_third_variable.cpp

---
+++ Code_for_swap_without_using_third_variable.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+	// your code goes here
+	int a,b;
+	cin>>a>>b;
+	a = a+b;
+	b = a-b;
+	a = a-b;
+	cout<<"a:"<<a<<endl;
+	cout<<"b:"<<b<<endl;
+	return 0;
+}
